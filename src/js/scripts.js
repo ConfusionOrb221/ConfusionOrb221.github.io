f5192769d38496222fef532799571561e460ae41
Nathan <nate1898@gmail.com>
2020-07-17 16:57:17 -0400

    Final Product

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,19 @@
 function menuFunction() {
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e;
     var menu = document.getElementsByClassName("menu");
+    var button = document.getElementsByClassName('collapsable');
     if (((_a = menu[0]) === null || _a === void 0 ? void 0 : _a.id) === "off") {
         (_b = menu[0]) === null || _b === void 0 ? void 0 : _b.setAttribute("id", "on");
+        (_c = button[0]) === null || _c === void 0 ? void 0 : _c.setAttribute("style", "position: fixed; right: 0;");
     }
     else {
-        (_c = menu[0]) === null || _c === void 0 ? void 0 : _c.setAttribute("id", "off");
+        (_d = menu[0]) === null || _d === void 0 ? void 0 : _d.setAttribute("id", "off");
+        (_e = button[0]) === null || _e === void 0 ? void 0 : _e.setAttribute("style", "position: relative;");
     }
 }
 window.onresize = doStuff;
 window.onload = doStuff;
+window.onscroll = checkHeader;
 var Header = document.createElement("span");
 Header.innerText = "Nathaniel Buckingham";
 function doStuff() {
@@ -22,5 +26,23 @@ function doStuff() {
         document.getElementById('header').innerHTML = "";
         document.getElementById('header').appendChild(Header);
         (_a = document.getElementById('header').firstChild) === null || _a === void 0 ? void 0 : _a.after(' Portfolio');
+    }
+}
+function checkHeader() {
+    var header = document.getElementById('head');
+    var banner = document.getElementById('banner');
+    var bounding = banner === null || banner === void 0 ? void 0 : banner.getBoundingClientRect();
+    if (!bounding || !(header === null || header === void 0 ? void 0 : header.clientHeight)) {
+        return null;
+    }
+    else if ((bounding === null || bounding === void 0 ? void 0 : bounding.bottom) < (200 - (header === null || header === void 0 ? void 0 : header.clientHeight)) && (header === null || header === void 0 ? void 0 : header.getAttribute('done')) !== 'true') {
+        header === null || header === void 0 ? void 0 : header.setAttribute('done', "true");
+        banner === null || banner === void 0 ? void 0 : banner.setAttribute("style", "margin-top: " + (header === null || header === void 0 ? void 0 : header.clientHeight));
+        header === null || header === void 0 ? void 0 : header.setAttribute("style", "\n            -webkit-transition: all 1s ease;\n            -moz-transition: all 1s ease;\n            -o-transition: all 1s ease;\n            transition: all 1s ease;\n            height: 10vh;\n            position: fixed;\n            top:0px;\n            z-index: 22; \n            background-color: #2a2f4abe;");
+    }
+    else if ((bounding === null || bounding === void 0 ? void 0 : bounding.bottom) > 100 && (header === null || header === void 0 ? void 0 : header.getAttribute('done')) !== 'false') {
+        header === null || header === void 0 ? void 0 : header.setAttribute('done', "false");
+        banner === null || banner === void 0 ? void 0 : banner.setAttribute("style", "margin-top: 0");
+        header === null || header === void 0 ? void 0 : header.setAttribute("style", "position: static; top:-50; z-index: 22");
     }
 }
--- src/js/scripts.ts
+++ src/js/scripts.ts
@@ -1,15 +1,19 @@
 function menuFunction() {
     const menu = document.getElementsByClassName("menu");
+    const button = document.getElementsByClassName('collapsable');
 
     if(menu[0]?.id === "off"){
         menu[0]?.setAttribute("id", "on")
+        button[0]?.setAttribute("style", "position: fixed; right: 0;")
     } else {
-        menu[0]?.setAttribute("id", "off")
+        menu[0]?.setAttribute("id", "off");
+        button[0]?.setAttribute("style", "position: relative;")
     }
 }
 
 window.onresize = doStuff;
 window.onload = doStuff;
+window.onscroll = checkHeader;
 
 const Header : HTMLSpanElement = document.createElement("span");
 Header.innerText = "Nathaniel Buckingham";
@@ -23,6 +27,38 @@ function doStuff(){
         (<HTMLElement>document.getElementById('header')).innerHTML = "";
         (<HTMLElement>document.getElementById('header')).appendChild(Header);
         (<HTMLElement>document.getElementById('header')).firstChild?.after(' Portfolio');
+    }
+}
+
+function checkHeader(){
+    const header = document.getElementById('head');
+    const banner = document.getElementById('banner');
+
+    const bounding = banner?.getBoundingClientRect();
+
+    if(!bounding || !header?.clientHeight){
+        return null;
+    }
+    else if(bounding?.bottom < (200 - header?.clientHeight) && header?.getAttribute('done') !== 'true'){
+        header?.setAttribute('done', "true");
+        banner?.setAttribute("style", `margin-top: ${header?.clientHeight}`);
+        header?.setAttribute(
+            "style", `
+            -webkit-transition: all 1s ease;
+            -moz-transition: all 1s ease;
+            -o-transition: all 1s ease;
+            transition: all 1s ease;
+            height: 10vh;
+            position: fixed;
+            top:0px;
+            z-index: 22; 
+            background-color: #2a2f4abe;`   
+        );
+    }
+    else if(bounding?.bottom > 100 && header?.getAttribute('done') !== 'false' ){
+        header?.setAttribute('done', "false");
+        banner?.setAttribute("style", "margin-top: 0");
+        header?.setAttribute("style", "position: static; top:-50; z-index: 22");
     }
 }
 
